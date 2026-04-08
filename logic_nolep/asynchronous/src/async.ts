6e31ccae65d1d42865a76f68c18cb1080624a3a6
Ayi <arifinrafli502@gmail.com>
2026-04-08 21:04:40 +0700

    update async

---
+++ logic_nolep/asynchronous/src/async.ts
@@ -0,0 +1,62 @@
+interface User {
+  id: number;
+  username: string;
+}
+
+type cb = (err: Error | null, result?: User) => void;
+
+const users: User[] = [
+  { id: 1, username: "jhon_doe" },
+  { id: 2, username: "jane_smith" },
+  { id: 3, username: "alice" },
+];
+
+const getUserDataCallback = (userId: number, callback: cb) => {
+  const user = users.find((u) => u.id === userId);
+  if (user) {
+    callback(null, user);
+  } else {
+    console.error(new Error("User not found"));
+  }
+};
+
+const getUserDataPromise = (userId: number) => {
+  return new Promise((resolve, reject) => {
+    const user = users.find((u) => u.id === userId);
+    if (!user) {
+      reject(new Error("User not found"));
+    }
+    resolve(user);
+  });
+};
+
+const getUserDataAsync = async (userId: number) => {
+  try {
+    const user = users.find((u) => u.id === userId);
+    if (user) {
+      return user;
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Test Case Callback
+getUserDataCallback(1, (err, user) => {
+  console.log("Callback Result:", user);
+});
+
+// Test Case Promise
+getUserDataPromise(2)
+  .then((user) => {
+    console.log("Promise Result:", user);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Test Case Async/Await
+(async () => {
+  const user = await getUserDataAsync(3);
+  console.log("Async/Await Result:", user);
+})();
